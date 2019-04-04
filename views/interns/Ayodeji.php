c85d7c4f5c00e6b76fa7ad358f4b07cf60eb4723
Ayodeji Ogundeji <ayodejivictor02@gmail.com>
2019-04-04 12:35:47 +0100

    My profile page

---
+++ views/interns/Ayodeji.php
@@ -0,0 +1,38 @@
+
+<html>
+   <?php include 'partials/header.php';?>
+   <body class=" pi">
+      <?php include 'partials/navbar.php';?>
+      <section class="main-section">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12 col-sm-6 col-xs-12">
+                  <div class="col-md-6 col-xs-12">
+                     <div class="intern-block border-blue">
+                        <img src="https://res.cloudinary.com/ay0dej1/image/upload/v1554310095/MY_PASSPORT.png" alt="Mark Essien">
+                        <h6>AYODEJI OGUNDEJI</h6>
+                        <p>Full Stack Web Developer</p>
+                        <ul class="list">
+                           <li><a href="#" target="_blank"<i class="fab fa-twitter fa-2x"></i></a></li>
+                           <li><a href="https://github.com/kiriyu" target="_blank"<i class="fab fa-github fa-2x"></i></a></li>
+                           <li><a href="https://medium.com/@Ay0dej1" target="_blank"<i class="fab fa-medium fa-2x"></i></a></li>
+                        </ul>
+                     </div>
+                  </div>
+                  <div class="col-md-6 col-xs-12 align-items-center">
+                     <div class="intern-bio border-blue">
+                        <h6>About me</h6>
+                        <p>Hi, I am an aspiring Full stack web developer based in Lagos, Nigeria. I hope to become a better developer through the HNG Internship.</p>
+                        <br />
+                        <h6>Skills</h6>
+                        <p>JavaScript | PHP | MySQL</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         </div>
+      </section>
+   </body>
+   <?php include 'partials/footer.php';?>
+</html>
